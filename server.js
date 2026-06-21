72f0c1301daa5ce290c5f466d40e4b89bb635b57
Adisakd29 <adisakd29@gmail.com>
2026-06-21 22:18:10 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ process.on('uncaughtException', (err) => {
 const authRoutes = require('./routes/auth');
 const wordsRoutes = require('./routes/words');
 const progressRoutes = require('./routes/progress');
+const translateRoutes = require('./routes/translate');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -27,6 +28,7 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/words', wordsRoutes);
 app.use('/api/progress', progressRoutes);
+app.use('/api/translate', translateRoutes);
 
 app.get('/api/health', (_req, res) => res.json({ ok: true }));
 
